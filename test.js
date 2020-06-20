d875305f6c37321a52061ca928ef6e26e5bfc802
Ilya <otto.cool@mail.ru>
2020-06-20 15:11:41 +0500

    first commit

---
+++ test.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//const answers = [];
+//answers[0] = prompt('Сколько тебе лет?', '');                   <-- Заполнение пустого массива
+//answers[1] = prompt('как тебя зовут?', '');                         по порядку.
+//answers[2] = confirm('всё хорошо?');
+//console.log(answers);
+
+//const category = 'toys';
+//console.log(`https://myneset.com/${category}/5`);                      <-- Интерполяция
+
+//const user = "Ivan";                                                   <--
+//alert(`Привет, ${user}`);                                              <--
+
+let inc = 10;
+    decr = 10;
+
+ //inc++;
+ //decr--;
+
+console.log(inc++);
